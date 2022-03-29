5112ba8d63c1b3efae493e92b16e12987a185622
BuildTools <unconfigured@null.spigotmc.org>
2022-03-29 14:28:41 -0700

    Added several new methods and tweaks * ask_for_move() takes user input to solve the first level * next_level() starts the next level * renamed clear_popup() to start_and_clear_popup() to be more clear * find_target() finds the end tile, fails if is the first level. * and more

--- Chroma.py
+++ Chroma.py
@@ -16,6 +16,9 @@ class Tile:
     def color(self):
         return Tile.get_rgb(self.tile.get_attribute("colorid"))
 
+    def click(self):
+        self.tile.click()
+
     @classmethod
     def get_rgb(self, rgb):
         rgb = re.search(r'rgb\((?P<red>\d{,3}), (?P<green>\d{,3}), (?P<blue>\d{,3})\)', rgb)
@@ -27,23 +30,41 @@ class Chroma:
         self.url = 'https://jessicaup.github.io/dull/'
         self.driver =  webdriver.Firefox()
         self.board = []
-        self.target_color = None
-        self.current_color = None
-        self.start_loca = [None, None]
+        self.turns = 0
+        self.start = {
+            "color": (None, None, None),
+            "loca": (None, None)
+        }
+        self.target = {
+            "color": (None, None, None),
+            "loca": (None, None)
+        }
 
 
-    def grab_target_and_currecnt(self):
-        self.current_color = Tile.get_rgb(self.driver.find_element(By.ID, 'current-color').get_attribute('style'))
+    def grab_target_and_start_colors(self):
+        self.start['color'] = Tile.get_rgb(self.driver.find_element(By.ID, 'current-color').get_attribute('style'))
 
-        self.target_color = Tile.get_rgb(self.driver.find_element(By.TAG_NAME, 'body').get_attribute('style'))
+        self.target['color'] = Tile.get_rgb(self.driver.find_element(By.TAG_NAME, 'body').get_attribute('style'))
         
 
-    def find_start_loca(self):
+    def find_start(self):
         for row in range(len(self.board)):
             for col in range(len(self.board)):
-                if self.board[row][col].color() == self.current_color:
-                    self.start_loca = (row, col)
+                tile = self.board[row][col]
+                if tile.color() == self.start['color']:
+                    self.start['loca'] = (row, col)
 
+
+    def find_target(self):
+        try:
+            star = self.driver.find_element(By.CLASS_NAME, 'star')
+        except:
+            star = None
+        if star:
+            parent_id = star.find_element(By.XPATH, './..').get_attribute('id')
+            row = int(parent_id[-2]) if parent_id[-2] != '-' else 0
+            col = int(parent_id[-1])
+            self.target['loca'] = (row, col)
 
     def grab_board(self):
         tiles = self.driver.find_elements(By.CLASS_NAME, 'mix-tile')
@@ -54,23 +75,57 @@ class Chroma:
             self.board.append(row)
 
 
-    def clear_popup(self):
+    def start_and_clear_popup(self):
         self.driver.get(self.url)
         self.driver.implicitly_wait(2)
         self.driver.find_element(By.CLASS_NAME, 'close-button').click()
+
 
     def close(self):
         self.driver.close()
 
 
+    def next_level(self, second_attempt=False):
+        self.driver.implicitly_wait(2)
+        try:
+            if not second_attempt:
+                self.driver.find_element(By.CLASS_NAME, 'level-button').click()
+        except:
+            print('something went wrong, trying again...')
+            self.next_level(True)
+
+
+    def ask_for_move(self):
+        print('please enter the answer for the first level')
+        while True:
+            move = input('-> ')
+            if move in ['up', 'down', 'left', 'right']:
+                break
+        if self.start['loca'][0] == None:
+            self.find_start()
+        row, col = self.start['loca'][0], self.start['loca'][1]
+        if move == 'up':
+            row -= 1
+        if move == 'down':
+            row += 1
+        if move == 'left':
+            col -= 1
+        if move == 'right':
+            col += 1
+        self.board[row][col].click()
+        self.next_level()
+        
+    
+
     def play(self, end_round):
-        self.clear_popup()
+        self.start_and_clear_popup()
         self.grab_board()
-        self.grab_target_and_currecnt()
-        self.find_start_loca()
-        print(self.start_loca)
-        # print('cur', self.current_color)
-        # print('tar', self.target_color)
+        self.grab_target_and_start_colors()
+        self.find_start()
+        self.ask_for_move()
+        self.find_target()
+        print(self.target['loca'])
+        # print(self.start['loca'])
         # print(self.board)
         # self.close()
 
